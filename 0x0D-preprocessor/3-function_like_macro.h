66383e06b2e1341a912addb1025e5d4566506aa2
EstherMumoJ <mutistaesther@gmail.com>
2023-07-17 05:59:43 -0700

    3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,9 +1,11 @@
-#ifndef THREE_FUNCTION_IN_ MIN_H
-#define THREE_FUNCTION_IN_ MIN_H
+#ifndef THREE_FUNCTION_LIKE_MACRO_H
+#define THREE_FUNCTION_LIKE_MACRO_H
 
 
 
-#define ABS(x)(x > 0 ? (x) : (x) * -1)
+#define ABS(x) (x > 0 ? (x) : (x) * -1)
 
 
-#endif /* THREE_FUNCTION_IN_ MIN_H */
+
+
+#endif /* THREE_FUNCTION_LIKE_MACRO_H */
