1ce1ce16d27f7fe44ce84cfe37cad504f81ef734
EstherMumoJ <mutistaesther@gmail.com>
2023-07-17 05:33:11 -0700

    file name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+/**
+ * main-name of the file the file is coppied from
+ * return - 0
+ * with a new line(/n)
+ */
+
+int main() 
+{ 
+	printf("program is from : %s\n", __PROGRAM__);
+	return 0;
+}
