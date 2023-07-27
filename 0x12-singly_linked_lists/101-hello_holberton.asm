41c18a40a00120eb9c55942606743a2e7a39af4b
EstherMumoJ <mutistaesther@gmail.com>
2023-07-27 04:22:32 -0700

    hello holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,12 +1,12 @@
-global main
-extern printf
-
 section .data
-    format db 'Hello, Holberton',0
+    format db 'Hello, Holberton', 0
 
 section .text
+    global main
+    extern printf
+
 main:
-    mov   edi, format
-    xor   eax, eax
-    call  printf
-    xor   eax, eax  ; Set eax to 0
+    mov edi, format
+    xor eax, eax
+    call printf
+    xor eax, eax  ; Set eax to 0
