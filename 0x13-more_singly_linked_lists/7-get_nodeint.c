2af1491d011db4a1c7e1640ab0e9730b0f585618
EstherMumoJ <mutistaesther@gmail.com>
2023-08-01 13:06:59 -0700

    7-get_nodeint.c

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+
+{
+	listint_t *current;
+	unsigned int count;
+
+	current = head;
+	count = 0;
+	while (count < index)
+	{
+		if (current)
+			current = current->next;
+		else
+			return (NULL);
+		count++;
+	}
+
+	if (current)
+		return (current);
+	else
+		return (NULL);
+}
