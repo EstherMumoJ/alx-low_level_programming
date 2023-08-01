91bfbbdf5e0d94535e6b9b505f23601f7e84f00a
EstherMumoJ <mutistaesther@gmail.com>
2023-08-01 13:00:05 -0700

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,13 +1,10 @@
 #include "lists.h"
 
 /**
- * free_listint2 - a fuction to free a `listint_t'
- *@head- the double pointer to the head
- */ 
-
+ * free_listint2 - free a `listint_t` list
+ * @head: double pointer to head
+ */
 void free_listint2(listint_t **head)
-
-
 {
 	listint_t *hold;
 
